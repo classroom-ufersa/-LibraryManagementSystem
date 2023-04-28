a4527d9eee582303a8d9a2cf77669cc1d3438fc7
fernandoClal <fernandclaudino12@gmail.com>
2023-04-28 14:32:38 -0300

    atualizei

---
+++ Library.c/library.c
@@ -0,0 +1,8 @@
+typedef struct biblioteca{
+    char nome[50];
+    int identificador;
+    char localizacao[50];
+    int numero_de_livro;
+    float hora_de_funionamento;
+    
+}Biblioteca;
--- Library.c/main.c
+++ Library.c/main.c
@@ -1,4 +1,6 @@
-# include<stdio.h>
+#include<stdio.h>
+#include"library.h"
+#include"Library.c"
 
 
 
